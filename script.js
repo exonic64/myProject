efbc871fb6b8727a15c8cb460f8254b1ee2d5420
Akim <akimsen12@gmail.com>
2019-11-30 13:51:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+  let leftBorderWidth = 1,
+      RightBorderWidth = 2;
+
+  let resul = leftBorderWidth + RightBorderWidth;
+
+alert(resul);
+alert("Привет");
+
+  let answer = confirm("Как твои дела?");
+console.log(answer);
+   let arr = [/* 0 */"sky.png"/* 0 */,/* 1 */"flower.jpg"/* 1 */,/* 2 */"rgb.png"/* 2 */];
+console.log(arr[1]);
+
